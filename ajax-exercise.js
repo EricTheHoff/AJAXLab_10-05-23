c34b794c1ffa8b914dfcf55c524305e1f344c25a
Eric Hoffman <eric.peter.hoffman@gmail.com>
2023-10-05 15:40:01 -0600

    Finished AJAX Lab

--- ajax-exercise.js
+++ ajax-exercise.js
@@ -2,17 +2,22 @@ import axios from 'axios';
 
 // PART 1: Show Dog Photo
 
-function showDogPhoto(evt) {
-  // TODO: get a random photo from the Dog API and show it in the #dog-image div
+async function showDogPhoto(evt) {
+  const url = 'https://dog.ceo/api/breeds/image/random'
+  const res = await axios.get(url)
+  const result = res.data.message
+  document.querySelector("#dog-image").innerHTML = `<img src=${result} alt="Random dog image"/>`
 }
 
 document.querySelector('#get-dog-image').addEventListener('click', showDogPhoto);
 
 // PART 2: Show Weather
 
-function showWeather(evt) {
-  const zipcode = document.querySelector('#zipcode-field').value;
-
+async function showWeather(evt) {
+  const zipcode = document.querySelector('#zipcode-field').value
+  const url = `/weather.txt?zipcode=${zipcode}`
+  const res = await axios.get(url)
+  document.querySelector("#weather-info").innerText = res.data
   // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
@@ -20,20 +25,43 @@ document.querySelector('#weather-button').addEventListener('click', showWeather)
 
 // PART 3: Order Cookies
 
-function orderCookies(evt) {
-  // TODO: Need to preventDefault here, because we're listening for a submit event!
-  // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+async function orderCookies(evt) {
+  evt.preventDefault()
+  const cookieType = document.querySelector("#cookie-type-field").value
+  const qty = document.querySelector("#qty-field").value
+
+  const res = await axios.post("/order-cookies.json", {
+    cookieType: cookieType,
+    qty: qty
+  })
+
+  document.querySelector("#order-status").innerText = res.data.message
+  // console.log(res.data)
+  const orderStatus = document.querySelector("#order-status")
+  if(res.data.resultCode === "ERROR") {
+    orderStatus.classList = "order-error"
+  }
 }
 document.querySelector('#order-form').addEventListener('submit', orderCookies);
 
 // PART 4: iTunes Search
 
-function iTunesSearch(evt) {
+async function iTunesSearch(evt) {
   evt.preventDefault();
-  const searchTerm = document.querySelector("#search-term").value;
+  const searchTerm = document.querySelector("#search-term").value
 
-  // TODO: In the #itunes-results list, show all results in the following format:
-  // `Artist: ${artistName} Song: ${trackName}`
+  const songData = {"term": searchTerm}
+  const queryString = new URLSearchParams(songData).toString()
+  const url = `https://itunes.apple.com/search?${queryString}&limit=10`
+
+  const res = await axios.get(url)
+
+  console.log(res.data)
+
+  let artistString = ""
+  for(const result of res.data.results) {
+    artistString += `<li>Artist: ${result.artistName} | Song: ${result.trackName}</li>`
+  }
+  document.querySelector("#itunes-results").innerHTML = artistString
 }
 document.querySelector('#itunes-search-form').addEventListener('submit', iTunesSearch);
--- app.js
+++ app.js
@@ -24,8 +24,13 @@ app.get('/', (req, res) => {
 
 app.get('/weather.txt', (req, res) => {
   const zipcode = req.query.zipcode;
-  // TODO: Get the weather for this zipcode and return the forecast if available.
-  // If not, return the default forecast.
+  const weatherZipKey = WEATHER[zipcode]
+
+  if(weatherZipKey) {
+    res.send(weatherZipKey.forecast)
+  } else {
+    res.send(DEFAULT_FORECAST)
+  }
 })
 
 app.post('/order-cookies.json', (req, res) => {
